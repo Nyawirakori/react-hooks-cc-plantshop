567e97622fbf0e28375b8a23565c1cb8e166bc12
Joan Nyawira Kori <korijoan171@gmail.com>
2025-04-25 17:06:44 +0300

    Get request made

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {//the prop(plant) passed from PlantPage..contains an array of plants
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {/* render PlantCards components in here */}
+      {plants.map((plant) => (
+         <PlantCard key={plant.id} plant={plant} />
+      ))}
+      </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -2,13 +2,34 @@ import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import {useEffect,useState} from "react";
 
 function PlantPage() {
+
+  const [plants,setPlants] = useState([]);
+
+//GET request-allow all the components to have acccess to plants
+async function getPlants(){
+  try{ //catching errors
+    const response = await fetch("http://localhost:6001/plants");//makes get request
+    const data = await response.json(); //converts the response to json format & saves it in a variable dat
+    setPlants(data); //updates states(plants) with the data fetched
+  }catch (error){
+    throw new Error(error);
+  }finally{ //used for cleanup, runs no matter what(error or not)
+  }
+}
+
+//runs some code after the component has been rendered
+useEffect(() =>{
+  getPlants();
+}, [])//([]) part is a dependancy array that ensures the effect runs once the component mounts
+//(appears on the screen for the first time)
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
